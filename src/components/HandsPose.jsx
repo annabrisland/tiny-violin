e0c870356272fa6be2b3531267240653887ce0c3
annabrisland <84166964+annabrisland@users.noreply.github.com>
2024-03-07 11:59:21 -0800

    Aligns drawn hand to video feed

--- src/components/HandsPose.jsx
+++ src/components/HandsPose.jsx
@@ -24,7 +24,6 @@ function HandsPose() {
           detectorConfig
         );
         setDetector(detector);
-        console.log("Hand Pose ready!");
       } catch (error) {
         console.log("Error initializing hand detection:", error);
       }
@@ -38,28 +37,24 @@ function HandsPose() {
   useEffect(() => {
     const runHandDetection = async () => {
       try {
-        const webcam = webcamRef.current.video;
         // Get & set video properties
-        const video = webcamRef.current.video;
-        const videoHeight = video.videoHeight;
-        const videoWidth = video.videoWidth;
+        const webcam = webcamRef.current.video;
+        const videoHeight = webcam.videoHeight;
+        const videoWidth = webcam.videoWidth;
 
         webcamRef.videoHeight = videoHeight;
         webcamRef.videoWidth = videoWidth;
 
         // Set canvas properties
-        canvasRef.videoHeight = videoHeight;
-        canvasRef.videoWidth = videoWidth;
-        const canvasContainer = document.querySelector(".canvas-wrapper");
-        canvasContainer.style = `width: ${videoWidth}px; height: ${videoHeight}px`;
+        canvasRef.current.height = videoHeight;
+        canvasRef.current.width = videoWidth;
 
         if (detector && webcam) {
-          const hands = await detector.estimateHands(webcam, {flipHorizontal: true});
+          const hands = await detector.estimateHands(webcam, {
+            flipHorizontal: true,
+          });
           console.log(hands);
           const ctx = canvasRef.current.getContext("2d");
-          var scale = window.devicePixelRatio;
-          canvasRef.current.width = Math.floor(videoWidth * scale);
-          canvasRef.current.height = Math.floor(videoHeight * scale);
           drawHands(hands, ctx);
         }
       } catch (error) {
@@ -67,7 +62,8 @@ function HandsPose() {
       }
     };
 
-    const intervalId = setInterval(runHandDetection, 100); // Run hand detection periodically
+    // Run hand detection periodically
+    const intervalId = setInterval(runHandDetection, 100); 
 
     return () => clearInterval(intervalId);
   }, [detector]);
@@ -75,69 +71,6 @@ function HandsPose() {
   const handleVideoLoaded = () => {
     setVideoLoaded(true);
   };
-
-  //   const webcamRef = useRef(null);
-  //   const canvasRef = useRef(null);
-
-  //   // Set Handpose model
-  //   useEffect(() => {
-  //       const initializeHandDetection = async () => {
-  //       try {
-  //         const model = handPoseDetection.SupportedModels.MediaPipeHands;
-  //         const detectorConfig = {
-  //           runtime: "mediapipe",
-  //           solutionPath: "https://cdn.jsdelivr.net/npm/@mediapipe/hands",
-  //           modelType: "full",
-  //           flipHorizontal: true,
-  //         };
-  //         const tempDetector = await handPoseDetection.createDetector(
-  //           model,
-  //           detectorConfig
-  //         );
-  //         setDetector(tempDetector);
-  //         console.log("Hand Pose Detection Loaded");
-  //       } catch (error) {
-  //         console.log("Error initializing hand detection:", error);
-  //       }
-  //   }
-  // initializeHandDetection()});
-
-  //   const runHandPose = async () => {
-  //     // Loop to continuously detect hands
-  //     setInterval(() => {
-  //       detect(detector);
-  //     }, 100);
-  //   };
-
-  //   const detect = async (net) => {
-  //     if (
-  //       typeof webcamRef !== "undefined" &&
-  //       webcamRef.current !== null &&
-  //       webcamRef.current.video.readyState === 4
-  //     ) {
-  //       // Get & set video properties
-  //       const video = webcamRef.current.video;
-  //       const videoHeight = video.videoHeight;
-  //       const videoWidth = video.videoWidth;
-
-  //       webcamRef.videoHeight = videoHeight;
-  //       webcamRef.videoWidth = videoWidth;
-
-  //       // Set canvas properties
-  //       canvasRef.videoHeight = videoHeight;
-  //       canvasRef.videoWidth = videoWidth;
-
-  //       // Detect hands
-  //       const hands = await net.estimateHands(video);
-  //       console.log(hands);
-  //       const ctx = canvasRef.current.getContext("2d");
-  //       drawHands(hands, ctx)
-  //     }
-  //   };
-
-  //   useEffect(() => {
-  //     runHandPose();
-  //   })
 
   return (
     <div>
@@ -153,16 +86,14 @@ function HandsPose() {
         audio={false}
         onUserMedia={() => handleVideoLoaded()}
       />
-      <div className="canvas-wrapper">
-        <canvas
-          className="video-feed canvas"
-          style={{
-            width: 640,
-            height: 480,
-          }}
-          ref={canvasRef}
-        />
-      </div>
+      <canvas
+        className="video-feed"
+        style={{
+          width: 640,
+          height: 480,
+        }}
+        ref={canvasRef}
+      />
     </div>
   );
 }
