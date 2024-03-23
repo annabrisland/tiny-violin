f7e643d49b8f145c6f98344ae029412cecca6fa2
annabrisland <84166964+annabrisland@users.noreply.github.com>
2024-03-22 19:16:04 -0700

    Simplify rollup Options

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,13 @@
 import { defineConfig } from 'vite'
-import {resolve} from 'path';
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
-        external: (source) => {
-            if (source.startsWith('.')) {
-                return false;
-            }
-
-            if (source.includes('node_modules')) {
-                return true;
-            }
-
-            return !source.includes(__dirname);
-        }
-    }
+      external: ['@tensorflow/tfjs-core'],
+    },
   },
   plugins: [react()],
 })
