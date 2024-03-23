df8196dc653437a9d25b1972d79b48b3f1c0386a
annabrisland <84166964+annabrisland@users.noreply.github.com>
2024-03-22 18:27:07 -0700

    Updates congfig

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
 })
