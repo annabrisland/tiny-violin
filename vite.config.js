c5b932885c86f612cfe9e6884a837162b8044b85
annabrisland <84166964+annabrisland@users.noreply.github.com>
2024-03-22 19:08:18 -0700

    Try extending build

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: './build',
-    rollupOptions: {
-      external: ['@tensorflow/tfjs-core'],
+    reportCompressedSize: false,
+    minify: false,
+    sourcemap: true,
+    outDir: 'dist',
+    lib: {
+        formats: ['es', 'cjs'],
+        entry: glob.sync(resolve(__dirname, 'src/**/*.{ts,tsx}')).reduce((entries, path) => {
+            if (path.endsWith('.d.ts')) {
+                return entries;
+            }
+
+            const outPath = path.replace(resolve(__dirname, 'src') + '/', '').replace(/\.(ts|tsx)$/, '');
+            entries[outPath] = path;
+            return entries;
+        }, {})
     },
+    commonjsOptions: {
+        include: [/packages/, /node_modules/]
+    },
+    rollupOptions: {
+        external: (source) => {
+            if (source.startsWith('.')) {
+                return false;
+            }
+
+            if (source.includes('node_modules')) {
+                return true;
+            }
+
+            return !source.includes(__dirname);
+        }
+    }
   },
   plugins: [react()],
   resolve: {
